136ae955654dd21b902e86884a54250f122d327b
oluchristian <oluchristian98@gmail.com>
2023-02-25 18:29:15 +0100

    first and uncompleted commit

---
+++ app/Http/Controllers/AdminController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Category;
+use Illuminate\Http\Request;
+
+class AdminController extends Controller
+{
+    public function view_category ()
+    {
+        $data = Category::all();
+        return view('Admin.category', compact('data'));
+    }
+
+    public function add_category (Request $request)
+    {
+         $data = new Category;
+         $data->category_name = $request->category;
+         $data->save();
+
+        return redirect()->back()->with('message', 'Categroy Added Successfully');
+    }
+
+    public function delete_category ($id)
+    {
+        $data = Category::find($id);
+        $data->delete();
+
+        return redirect()->back()->with('message', 'Categroy Deleted Successfully');
+    }
+}
